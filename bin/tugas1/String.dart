3c354945f3bcae2ac213dd2510253e1d202d6af1
charryy04 <amaliakasihchary@gmail.com>
2024-10-03 21:20:57 +0800

    String

---
+++ bin/tugas1/String.dart
@@ -0,0 +1,85 @@
+//string
+// void main() {
+//   String firstName = 'Chary';
+//   String lastName = "Amalia";
+
+//   print(firstName);
+//   print(lastName);
+// }
+
+//String Interpolation
+// void main(){
+//   String firstName = 'Chary';
+//   String lastName = "Amalia";
+
+//   print(firstName);
+//   print(lastName);
+
+//   var fullName = '$firstName ${lastName}';
+//   print(fullName);
+// }
+
+//Karakter Backslah
+// void main() {
+//   String firstName = 'Chary';
+//   String lastName = "Amalia";
+
+//   print(firstName);
+//   print(lastName);
+
+//   var fullName = '$firstName ${lastName}';
+//   print(fullName);
+
+//   var text = 'this is \'dart\' \$cool';
+//   print(text);
+// }
+
+//Menggabungkan String
+// void main() {
+//   String firstName = 'Chary';
+//   String lastName = "Amalia";
+
+//   print(firstName);
+//   print(lastName);
+
+//   var fullName = '$firstName ${lastName}';
+//   print(fullName);
+
+//   var text = 'this is \'dart\' \$cool';
+//   print(text);
+
+//   var name1 = firstName + " " + lastName;
+//   var name2 = 'Chary' ' Amalia' ' Kasih';
+
+//   print(name1);
+//   print(name2);
+// }
+
+//Multiline String
+void main() {
+  String firstName = 'Chary';
+  String lastName = "Amalia";
+
+  print(firstName);
+  print(lastName);
+
+  var fullName = '$firstName ${lastName}';
+  print(fullName);
+
+  var text = 'this is \'dart\' \$cool';
+  print(text);
+
+  var name1 = firstName + " " + lastName;
+  var name2 = 'Chary' ' Amalia' ' Kasih';
+
+  print(name1);
+  print(name2);
+
+  var longstring = '''
+this is long string
+multiline string
+learning dart
+  ''';
+
+  print(longstring);
+}
