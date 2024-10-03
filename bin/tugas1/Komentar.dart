15337c4d88d99330c3041b58a059bd163947efce
charryy04 <amaliakasihchary@gmail.com>
2024-10-03 14:31:06 +0800

    Komentar

---
+++ bin/tugas1/Komentar.dart
@@ -0,0 +1,7 @@
+//Jika ingin memberi komentar hanya satu baris caranya dengan menggunakan 2 garis miring atau Single-Line
+/*Tanda ini digunakan jika lebih dari satu baris yang disebut Multi-Line*/
+///Digunakan untuk dart documentation 
+void main() {
+  var name = 'Chary Amalia Kasih';
+  print(name);
+}
