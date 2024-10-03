680094818bc90644de127bcab207def0bfd13ac8
charryy04 <amaliakasihchary@gmail.com>
2024-10-03 13:14:10 +0800

    tugas variabel nama

--- bin/hello.dart
+++ bin/hello.dart
@@ -1,7 +1,3 @@
 void main() {
-  int number = 42;
-  number += 99;
-  number -= 1;
-  number = 2;
-  print('Hello, $number');
+  print('Hello, world!');
 }
---
+++ bin/tugas1/Variabel1.dart
@@ -0,0 +1,6 @@
+void main() {
+  String name = 'Chary Amalia Kasih';
+  name = 'Chary Amalia Kasih';
+
+  print(name);
+}
