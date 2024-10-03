f4ac22c0110cb7c174500c88d60a9fa13e1542b4
charryy04 <amaliakasihchary@gmail.com>
2024-10-03 20:53:12 +0800

    Number

---
+++ bin/tugas1/Number.dart
@@ -0,0 +1,32 @@
+//Tipe Data Number
+// void main() {
+//   int number1 = 10;
+//   double number2 = 10.5;
+
+//   print(number1);
+//   print(number2);
+// }
+
+//Tipe Data Num
+// void main() {
+//   int number1 = 10;
+//   double number2 = 10.5;
+
+//   print(number1);
+//   print(number2);
+
+//   num number = 10;
+//   print(number);
+
+//   number = 10.5;
+//   print(number);
+// }
+
+//Tipe Data Boolean
+void main() {
+  bool finish = false;
+  print(finish);
+
+  finish = true;
+  print(finish);
+}
