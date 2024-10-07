2315bcede358b504320276cb0cb2a2ce3b66434e
charryy04 <amaliakasihchary@gmail.com>
2024-10-07 21:01:40 +0800

    List

---
+++ bin/tugas1/List.dart
@@ -0,0 +1,45 @@
+// Ini contoh membuat List
+// void main(){
+//   List<int> listinteger = [];
+//   var listString = <String>[];
+// }
+
+// Ini cara memasukan atau menambahkan Value di dalam List
+// void main(){
+//   var nama = <String>[];
+
+//   nama.add('Chary');
+//   nama.add('Amalia');
+//   nama.add('Kasih');
+
+//   print(nama);
+//   print(nama.length);
+// }
+
+// Ini contoh Memanipulasi Data di dalam List
+// void main(){
+//   var nama = <String>[];
+
+//   nama.add('Chary');
+//   nama.add('Amalia');
+//   nama.add('Kasih');
+
+//   print(nama);
+//   print(nama.length);
+
+//   print(nama[0]);
+//   nama[0] = 'Aku';
+//   nama.removeAt(2);
+//   print(nama);
+// }
+
+// Cara membuat List dengan deklarasi secara langsung
+void main() {
+  var namaOrang = ['Chary', 'Jimmy', 'Bass'];
+  var umurOrang = <int>[22, 30, 28];
+  var nilaiOrang = [90, 95, 90];
+
+  print(namaOrang);
+  print(umurOrang);
+  print(nilaiOrang);
+}
