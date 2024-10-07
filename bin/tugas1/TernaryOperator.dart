ef66c52591401e3d3085c77f12acc34644a40fe8
charryy04 <amaliakasihchary@gmail.com>
2024-10-07 21:35:01 +0800

    TernaryOperator

---
+++ bin/tugas1/TernaryOperator.dart
@@ -0,0 +1,22 @@
+// Ini contoh yang Tidak Memakai Ternary Operator
+// void main(){
+//   var nilai = 90;
+//   String katakata;
+
+//   if(nilai >= 70){
+//     katakata = 'Selamat Nilai Anda Bagus Sekaliiii';
+//   }else{
+//     katakata = 'Maaf Anda Harus Belajar Lagi';
+//   }
+
+//   print(katakata);
+// }
+
+// Ini contoh yang Memakai Ternary Operator
+void main(){
+  var nilai = 95;
+  var katakata = nilai >=75 ? 'Selamat Anda Berhasillll':'Maaf Anda Harus Lebih Giat Belajar';
+  
+  print(katakata);
+}
+
