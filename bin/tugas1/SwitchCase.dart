38dd960691d57f8a350e5ebb9c3b604f8297580b
charryy04 <amaliakasihchary@gmail.com>
2024-10-07 21:26:15 +0800

    SwitchCase

---
+++ bin/tugas1/SwitchCase.dart
@@ -0,0 +1,19 @@
+// Ini Switch Case
+void main(){
+  var nilai = 'E';
+
+  switch(nilai){
+    case 'A':
+    print('Wahhh Selamat Anda LULUS dengan Baik Sekaliiii');
+    break;
+    case 'B':
+    case 'C':
+    print('Selamat Anda LULUS dengan Baik');
+    break;
+    case 'D':
+    print('Maaf Anda TIDAK LULUS');
+    break;
+    default:
+    print('Maaf Anda TIDAK LULUS Jangan Putus Asa dan Tetap Semangat Yaaa');
+  }
+}
