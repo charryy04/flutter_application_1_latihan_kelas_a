022916d304ddcc2be518da1d96be5935ccdc383d
charryy04 <amaliakasihchary@gmail.com>
2024-10-07 20:36:19 +0800

    KonversiTipeData

---
+++ bin/tugas1/KonversiTipeData.dart
@@ -0,0 +1,22 @@
+// // Ini adalah contoh Konversi Tipe Data Int, Double, dan String
+// void main(){
+//   var masukkanstring = '50';
+//   var masukanInt = int.parse(masukkanstring);
+//   var masukanDouble = double.parse(masukkanstring);
+
+//   var doubledariInt = masukanInt.toDouble();
+//   var intdariDouble = masukanDouble.toInt();
+
+//   var stringdariInt = masukanInt.toString();
+//   var stringdariDouble = masukanDouble.toString();
+// }
+
+// Ini adalah contoh KOnversi Tipe Data Boolean ke String
+void main() {
+  var masukkanstring = 'true';
+  var masukanBoolean = masukkanstring == 'true';
+
+  var stringdariBoolean = masukanBoolean.toString();
+
+  print(stringdariBoolean);
+}
