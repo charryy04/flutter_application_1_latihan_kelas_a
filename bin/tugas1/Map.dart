5317bf9545665a3dd6a30c35face8fa210a87abc
charryy04 <amaliakasihchary@gmail.com>
2024-10-07 21:10:49 +0800

    Map

---
+++ bin/tugas1/Map.dart
@@ -0,0 +1,37 @@
+// Ini cara membuat Map
+// void main(){
+//   Map <String, String> nama ={};
+//   var nama1 = Map<String, String>();
+//   var nama2 = <String, String>{};
+
+//   print(nama);
+//   print(nama1);
+//   print(nama2);
+// }
+
+// Ini cara memanipulasi Map
+// void main(){
+//   var namaKu = <String, String>{};
+//   namaKu['awal'] = 'Chary';
+//   namaKu['tengah'] = 'Amalia';
+//   namaKu['akhir'] = 'Kasih';
+
+//   print(namaKu['awal']);
+
+//   namaKu['tengah'] = 'Amalia';
+//   print(namaKu);
+
+//   namaKu.remove('akhir');
+//   print(namaKu);  
+// }
+
+// Ini mendeklarasikan Map secara langsung
+void main(){
+  var nama = {
+    'awal':'Chary',
+    'tengah':'Amalia',
+    'akhir':'Kasih'
+  };
+
+  print(nama['awal']);
+}
