290d9e493a9bf33daa187eafbd2dee726434b392
charryy04 <amaliakasihchary@gmail.com>
2024-10-09 23:29:17 +0800

    ForLoop

---
+++ bin/tugas1/ForLoop.dart
@@ -0,0 +1,33 @@
+//Ini Perulangan Tanpa Henti
+// void main() {
+//   for (;;) {
+//     print('Perulangan Tanpa Henti');
+//   }
+// }
+
+//Ini Perulangan Dengan Kondisi
+// void main() {
+//   var counter = 1;
+//   for (; counter <= 10;) {
+//     print('Perulangan ke-$counter');
+
+//     counter++;
+//   }
+// }
+
+//Ini Perulangan Dengan Init Statement
+// void main() {
+//   for (var counter = 1; counter <= 10;) {
+//     print('Perulangan ke-$counter');
+
+//     counter++;
+//   }
+// }
+
+//Ini Perulangan Dengan Post Statement
+void main() {
+  for (var counter = 1; counter <= 10; counter++) {
+    print('Perulangan ke-$counter');
+
+  }
+}
