806d47bcd1ce96ded043ef544de7c5500680bb7c
charryy04 <amaliakasihchary@gmail.com>
2024-10-10 00:00:48 +0800

    FunctionParameter

---
+++ bin/tugas1/FunctionParameter.dart
@@ -0,0 +1,9 @@
+//Ini Function Parameter
+void sayHello(String firstName, String lastName) {
+  print('Hello $firstName $lastName');
+}
+
+void main() {
+  sayHello('CHARY', 'AMALIA');
+  sayHello('JIMMY', 'BASS');
+}
