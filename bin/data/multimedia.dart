aa28d16b337353f183fdae3ddb90be6c3ecfc4ee
charryy04 <amaliakasihchary@gmail.com>
2024-10-22 18:19:06 +0800

    Mixin

---
+++ bin/data/multimedia.dart
@@ -0,0 +1,32 @@
+//Ini Membatasi Mixin
+abstract class Multimedia {
+
+}
+
+mixin Playable on Multimedia {
+  String? name;
+
+  void play() {
+    print('Play $name');
+  }
+}
+
+mixin Stoppable on Multimedia {
+  String? name;
+
+  void stop() {
+    print('Stop $name');
+  }
+}
+
+//Ini Kode Menggunakan Mixin 
+class Video extends Multimedia with Playable, Stoppable{
+
+}
+
+class Audio extends Multimedia with Playable, Stoppable{
+
+}
+
+
+
