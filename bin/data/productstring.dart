f0d337469fd041fe3708d1e316bb2746aeb89a06
charryy04 <amaliakasihchary@gmail.com>
2024-10-22 18:29:14 +0800

    ToStringMethod

---
+++ bin/data/productstring.dart
@@ -0,0 +1,15 @@
+class Product {
+  String? id;
+  String? name;
+  int? _quantity; //Tidak bisa diakses diluar directory
+
+  int? getQuantity() {
+    return _quantity;
+  }
+
+  String toString() {
+    return "Product{id=$id, name=$name, quantity=$_quantity}";
+  }
+}
+
+
