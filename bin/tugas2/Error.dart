5f206f44fd0c1c39b4148845668f4a59c1077feb
charryy04 <amaliakasihchary@gmail.com>
2024-10-25 22:05:20 +0800

    Error

---
+++ bin/tugas2/Error.dart
@@ -0,0 +1,17 @@
+// //Error
+void main() {
+  var list = ["CHARY", "AMALIA", "KASIH"];
+  print(list[10]);
+}
+
+
+//Ini Tidak Error
+// void main() {
+//   var list = ["CHARY", "AMALIA", "KASIH"];
+
+//   if (list.length > 10) {
+//     print(list[10]);
+//   }
+// }
+
+
